b919ffed564b9078aa9998e122fc8021ba185c65
Alfonso Pappalardo <alfonso.pappalardo.1996@gmail.com>
2019-07-26 17:46:41 +0200

    commit

--- src/b04/s40/For.java
+++ src/b04/s40/For.java
@@ -2,14 +2,16 @@ package b04.s40;
 
 public class For {
     public static void main(String[] args) {
-        for (int i = 0; i < 5; i++) {
-            System.out.println("Begin block loop " + i);
+        String[] array = new String[5];
+        
+        for (int i = 0; i < array.length; i++) {
+        	array[i] = "";
+            
+        }
 
-            if (i == 2) {
-                continue;
-            }
-
-            System.out.println("End block loop " + i);
+        for (String item : array) {
+        	System.out.println(item.length());
         }
     }
+    
 }
