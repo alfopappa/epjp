41d8c54333a30521b292b4158c68f964fe967210
Alfonso Pappalardo <alfonso.pappalardo.1996@gmail.com>
2019-07-31 10:35:35 +0200

    ricordarsi di rifarlo secondo il proprio meccanismo in MyS87

---
+++ src/b08/MyS87.java
@@ -0,0 +1,37 @@
+package b08;
+
+import java.util.Scanner;
+
+public class MyS87 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Please, enter some int numbers");
+
+		while (scanner.hasNextInt()) {
+			scanner.next();
+			System.out.println("Enter at least one int number");
+		}
+		int n;
+		int cont = 1;
+		int sum = 0;
+		double average = 0;
+
+		while (scanner.hasNextInt()) {
+			n = scanner.nextInt();
+			cont++;
+			sum = sum + n;
+		}
+
+		average = sum / cont;
+//		int result = 0;
+//   	 
+//   	 for(int i = 0; i < scanner.length; i = i+1) {
+//   	      result = result + scanner[i];
+//   	    
+//
+//   	    result = result / scanner.length;
+
+		System.out.println("The average value is: " + average);
+	}
+
+}
